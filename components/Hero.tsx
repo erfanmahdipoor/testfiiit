91c5780c31be0a7b3aa7d918a6e06227994b5c39
erfan <erfanmhdipoor@gmail.com>
2024-06-09 15:28:16 +0330

    add:test one

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,49 @@
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import Magicbutton from "./ui/Magicbutton";
+import Link from "next/link";
+import { FaLocationArrow } from "react-icons/fa";
 const Hero = () => {
-  return <div className="pb-20 Pt-36">Hero</div>;
+  return (
+    <div className="pb-96 pt-48">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top20  h-screen  "
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vw]  "
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]  " fill="blue" />
+      </div>
+
+      <div>
+        <div className="h-screen w-full dark:bg-black bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0  left-0">
+          {/* Radial gradient for the container to give a faded look */}
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+        </div>
+      </div>
+      <div className="flex justify-center relative my-20  z-10">
+        <div className="max-w-[89vw] md:max-w-2xl ld:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="uppercase  tracking-widest text-xs text-center text-blue-100 max-w-80">
+            dynamic web amgic
+          </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="fitaminC is a best website for create your body and improve your life"
+          />
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            hi im p
+          </p>
+
+          <Link href="/">
+            <Magicbutton title="get started" icon={<FaLocationArrow/>} position="right"/>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
